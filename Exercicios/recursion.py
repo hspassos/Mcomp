54f3cb55cfb0dd057e34068cea119d6659164602
hspassos <passos.487@gmail.com>
2023-07-03 17:34:24 -0300

    testes no exercício de recursão

--- Exercicios/recursion.py
+++ Exercicios/recursion.py
@@ -1,18 +1,21 @@
 a = [2, 4, 6, 8, 10, 12, 14]
-x = 12
+x = 2
 def binary_search(a, x, lower=0):
-    if lower >= len(a):
+
+    if lower > len(a) - 1:
         return -1
-
-    mid = (lower + len(a)) // 2
-
-    if a[mid] == x:
-        return mid
-    elif a[mid] < x:
-        return binary_search(a, x, mid + 1)
     else:
-        return binary_search(a, x, lower, mid - 1)
+        middle = (lower + len(a) - 1) // 2
+
+        if x == a[middle]:
+            return middle
+        elif x < a[middle]:
+            return binary_search(a, x, middle - 1)
+        else:
+            return binary_search(a, x, middle + 1)
 
 result = binary_search(a, x)
 
 print(result)
+
+#print(a[len(a) // 2]) = 8
